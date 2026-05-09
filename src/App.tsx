ef078630428397f890154e470f6918798794e05b
Dmytro Farbun <dimofarb@gmail.com>
2026-05-09 19:09:31 +0300

    feat: implement Header and Button components, update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  return <></>;
+  return (
+    <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
